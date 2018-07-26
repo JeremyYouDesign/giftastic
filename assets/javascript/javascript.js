86a84255645dd487cc978ea21848b1577f4f58e3
Jeremy <jeremyyoudesign@gmail.com>
2018-07-26 12:08:09 -0400

    .

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,71 @@
+var queries = ["mariah carey", "britney spears", "lady gaga", "lana del ray", "gwen stefani", "taylor swift"];
+var searchQuery;
+
+function displayGif() {
+    var query = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=QxuyXh3t1krkKZpPg1nNAa5fxYTQJae0&limit=10";
+    console.log("You searched " + query);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        var results = response.data;
+    
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div class='item'>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var image = $("<img>");
+            image.addClass("gif");
+            image.attr({
+                "data-state": "still",
+                "stillURL": results[i].images.fixed_height_still.url,
+                "animateURL": results[i].images.fixed_height.url
+            });
+                
+            gifDiv.append(image.attr("src", image.attr("stillURL")));
+            gifDiv.append(p);
+    
+            $("#gif-div").prepend(gifDiv);
+        }
+    });
+}
+
+var renderButton = function() {
+    $("#button-div").empty();
+
+    for (var i = 0; i < queries.length; i++) {
+        var a = $("<button>");
+        a.addClass("btn btn-info gif-btn");
+        a.attr("data-name", queries[i]);
+        a.text(queries[i]);
+        $("#button-div").append(a);        
+    }
+}
+
+$("#search-btn").on("click", function(event) {
+    event.preventDefault();
+    var searchQuery = $("#search-form").val().trim();
+    queries.push(searchQuery);
+
+    renderButton();
+});
+
+$(document).on("click", ".gif-btn", displayGif);
+
+$(document).on("click", ".gif", function() {
+    var state = $(this).attr("data-state");
+    console.log(state);
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("animateURL"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("stillURL"));
+        $(this).attr("data-state", "still");
+    }
+});
+
+renderButton();
+console.log(queries);
